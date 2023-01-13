6879952a75893c9b5ae4182de3087355f372f8a1
Sergey Kievskiy <knhel7@mail.ru>
2023-01-13 18:55:11 +0300

    main edited

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ from email_agent import MailAgent
 
 
 if __name__ == '__main__':
-    gmail_agent = MailAgent('kievskiy.s@gmail.com')
-    gmail_agent.send_message(recipients=['knhel7@gmail.com', 'knhel7@mail.ru'])
+    gmail_agent = MailAgent('my@example.com')
+    gmail_agent.send_message(recipients=['user7@forexample.com', 'user7@example.ru'])
     gmail_agent.receive_message()
